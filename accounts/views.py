f87afcc7e1cabc2cdc12b21d74e0eb1ed781a39b
Taegyun Lee <taegyun@MacBookPro2x-gray.local>
2023-04-19 16:53:45 +0900

    project init

---
+++ accounts/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from .models import User
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('posts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('posts:index')
